087f0081769c44883abfcd3107a01a0080ba858c
David Andersson <lejonmanen@gmail.com>
2020-05-11 16:39:42 +0200

    Modified example to use CSS animations

--- script.js
+++ script.js
@@ -1,30 +1,41 @@
 window.addEventListener('load', () => {
     console.log('Page loaded');
+    const url = 'https://api.sunrise-sunset.org/json';
 
     let buttonGetData = document.querySelector('#getData');
-    buttonGetData.addEventListener('click', makeApiRequest);
+    let buttonError   = document.querySelector('#getError');
+    buttonGetData.addEventListener('click', () => makeApiRequest(url));
+    buttonError.addEventListener('click', () => makeApiRequest(url + 'pp'));
 })
 
-async function makeApiRequest() {
-    const url = 'https://api.sunrise-sunset.org/json';
+async function makeApiRequest(url) {
     let lat = '57';
     let lng = '11';
     let qs = `${url}?lat=${lat}&lng=${lng}`;
     showStatus('About to make API request with URL=' + qs);
 
-    const response = await fetch(qs);
-    const json = await response.json();
-    if( json.status === 'OK' ) {
-        showStatus('Got response from API');
-        let sunrise = json.results.sunrise;
-        let sunset  = json.results.sunset;
-        outputResult(`The sun rose at ${sunrise} today and will set at ${sunset}.`);
-    } else {
-        // TODO: not user friendly to show cryptic error messages - change this
-        showStatus('API returned error: ' + json.status);
+    resetError();
+    try {
+        const response = await fetch(qs);
+        const json = await response.json();
+        if( json.status === 'OK' ) {
+            showStatus('Got response from API');
+            let sunrise = json.results.sunrise;
+            let sunset  = json.results.sunset;
+            outputResult(`The sun rose at ${sunrise} today and will set at ${sunset}.`);
+            console.log(json);
+
+        } else {
+            // TODO: not user friendly to show cryptic error messages - change this
+            showStatus('API returned error: ' + json.status);
+            outputResult(``);
+            flashError();
+        }
+    } catch {
+        showStatus('Fetch completely failed.');
         outputResult(``);
+        flashError();
     }
-    console.log(json);
 }
 
 function showStatus(text) {
@@ -36,4 +47,13 @@ function showStatus(text) {
 function outputResult(text) {
     let p = document.querySelector('.answer');
     p.innerText = text;
+}
+
+function flashError() {
+    let status = document.querySelector('.status');
+    status.classList.add('flash-error');
+}
+function resetError() {
+    let status = document.querySelector('.status');
+    status.classList.remove('flash-error');
 }
